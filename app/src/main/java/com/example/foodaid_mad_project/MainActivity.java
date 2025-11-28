b688590c679f5ab6129c74150e6773aa7078b34d
IK <ingkhing05@gmail.com>
2025-11-28 21:02:21 +0800

    Added Splash Screen

--- app/src/main/java/com/example/foodaid_mad_project/MainActivity.java
+++ app/src/main/java/com/example/foodaid_mad_project/MainActivity.java
@@ -1,24 +1,49 @@
 package com.example.foodaid_mad_project;
 
+import android.animation.ObjectAnimator;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.AnticipateInterpolator;
 
-import androidx.activity.EdgeToEdge;
+import androidx.core.splashscreen.SplashScreen;
+
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        SplashScreen splashScreen = SplashScreen.installSplashScreen(this);
+
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        long animationDuration = 2100;
+        long startTime = System.currentTimeMillis();
+        splashScreen.setKeepOnScreenCondition(() -> {
+            long elapsedTime = System.currentTimeMillis() - startTime;
+            return elapsedTime < animationDuration;
+        });
+
+        splashScreen.setOnExitAnimationListener(splashScreenViewProvider -> {
+            final View splashScreenView = splashScreenViewProvider.getView();
+            final ObjectAnimator slideUp = ObjectAnimator.ofFloat(
+                    splashScreenView,
+                    View.TRANSLATION_Y,
+                    0f,
+                    -splashScreenView.getHeight()
+            );
+            slideUp.setInterpolator(new AnticipateInterpolator());
+            slideUp.setDuration(300L);
+
+            slideUp.addListener(new android.animation.AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(android.animation.Animator animation) {
+                    splashScreenViewProvider.remove();
+                }
+            });
+
+            // Run your animation.
+            slideUp.start();
         });
     }
 }
