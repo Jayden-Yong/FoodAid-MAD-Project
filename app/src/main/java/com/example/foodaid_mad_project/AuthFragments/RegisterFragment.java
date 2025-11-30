dade3ed61a22062ea670ab2c468b60d16c693197
IK <ingkhing05@gmail.com>
2025-11-30 15:21:38 +0800

    Created xml of Authentication Module based on Figma

---
+++ app/src/main/java/com/example/foodaid_mad_project/AuthFragments/RegisterFragment.java
@@ -0,0 +1,6 @@
+package com.example.foodaid_mad_project.AuthFragments;
+
+import androidx.fragment.app.Fragment;
+
+public class RegisterFragment extends Fragment {
+}
--- app/src/main/java/com/example/foodaid_mad_project/LauncherActivity.java
+++ app/src/main/java/com/example/foodaid_mad_project/LauncherActivity.java
@@ -3,5 +3,5 @@ package com.example.foodaid_mad_project;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class LauncherActivity extends AppCompatActivity {
-
+    //Check authentication while splash screen is running
 }
