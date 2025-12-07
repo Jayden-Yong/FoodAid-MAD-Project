6b459098773ab9e8d77decc686d16fc536a31b15
IK <ingkhing05@gmail.com>
2025-12-07 23:19:20 +0800

    Check again for the merge

--- app/src/main/java/com/example/foodaid_mad_project/LauncherActivity.java
+++
@@ -1,7 +0,0 @@
-package com.example.foodaid_mad_project;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class LauncherActivity extends AppCompatActivity {
-    //Check authentication while splash screen is running
-}
