09560e5567d5b7bd7f589bc5de7e5e19ddead2d2
IK <ingkhing05@gmail.com>
2025-12-11 22:00:08 +0800

    Add java for community and profile fragment

---
+++ app/src/main/java/com/example/foodaid_mad_project/CommunityFragments/CommunityFragment.java
@@ -0,0 +1,18 @@
+package com.example.foodaid_mad_project.CommunityFragments;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.foodaid_mad_project.R;
+
+public class CommunityFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_community, container, false);
+    }
+}
